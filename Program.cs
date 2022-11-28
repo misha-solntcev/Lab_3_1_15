63ccb39352491887f57ca52a7f9ff788beeff724
misha-solntcev <misha.solntcev@gmail.com>
2022-11-28 18:31:11 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  15. Начальный вклад в банке равен 1000 руб. Через каждый месяц 
+    размер вклада увеличивается на P процентов от имеющейся суммы 
+    (P — вещественное число, 0 < P < 25). По данному P определить, 
+    через сколько месяцев размер вклада превысит 1100 руб., 
+    и вывести найденное количество месяцев K (целое число) и итоговый 
+    размер вклада S (вещественное число). */
+
+namespace Lab_3_1_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vklad = 1000;
+            double P = 1;
+            int month = 0;
+            while (vklad < 1100)
+            {
+                month++;
+                vklad = vklad * (1 + P / 100);
+                Console.WriteLine($"month = {month}, vklad = {vklad}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
